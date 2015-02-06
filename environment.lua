c43b0ef6dc256a15cf7978351b871f3d93ddfb8e
Tim <t4im@users.noreply.github.com>
2015-02-06 14:38:05 +0100

    move the undiggable node to its test

--- environment.lua
+++ environment.lua
@@ -1,6 +1,14 @@
 local test_position = {x=51,y=1,z=50}
 local original_protection = minetest.is_protected
 
+minetest.register_node("api_test:undiggable", {
+	description = "node that doesn't allow to be dug by players",
+	tiles = {"default_lava.png"},
+	groups = { dig_immediate=2 },
+	paramtype = "light",
+	can_dig = function(pos, player) return false end
+})
+
 -- minetest.dig_node(pos)
 -- ^ Dig node with the same effects that a player would cause
 --   Returns true if successful, false on failure (e.g. protected location)
--- init.lua
+++ init.lua
@@ -1,13 +1,5 @@
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
-minetest.register_node("api_test:undiggable", {
-	description = "node that doesn't allow to be dug by players",
-	tiles = {"default_lava.png"},
-	groups = { dig_immediate=2 },
-	paramtype = "light",
-	can_dig = function(pos, player) return false end
-})
-
 dofile(modpath .. "/data_handling.lua")
 dofile(modpath .. "/item_handling.lua")
 dofile(modpath .. "/environment.lua")
