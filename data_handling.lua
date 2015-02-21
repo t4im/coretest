4553879a483abf828024b2b16192e415c338b203
Tim <t4im@users.noreply.github.com>
2015-02-21 20:42:25 +0100

    update/add unittest for minetest/minetest#2365

--- data_handling.lua
+++ data_handling.lua
@@ -7,12 +7,31 @@ describe("minetest.serialize(table)", function()
 		Then("deserialize back to the original version")
 		assert.are.same(test_table, minetest.deserialize(serialized_table))
 	end)
-	it("serializes nested tables correctly (minetest/minetest#2365)", function()
+	it("serializes nested tables correctly", function()
 		Given("a table with a nested table")
 		local test_table = {
 			nested_table = {
-				nested_value = 23
+				nested_value = 23,
 			}
+		}
+		When("serializing the table")
+		local serialized_table = minetest.serialize(test_table)
+		Then("deserialize back to the original version")
+		assert.are.same(test_table, minetest.deserialize(serialized_table))
+	end)
+	it("serializes floating point numbers correctly (minetest/minetest#2365)", function()
+		Given("a table with floating point values and key")
+		local test_table = {
+			float1 = 1.1234567891234,
+			float3 = 1234567891234.1,
+			[1.1234567891234] = true,
+			[1234567891234.1] = true,
+			-- persisting anything with a higher precision than 14 seems currently unfeasible
+			-- even though lua handles floating points in ram with a higher precision
+			-- float2 = 12.1234567891234,
+			-- float4 = 1234567891234.12,
+			-- [12.1234567891234] = true,
+			-- [1234567891234.12] = true,
 		}
 		When("serializing the table")
 		local serialized_table = minetest.serialize(test_table)
